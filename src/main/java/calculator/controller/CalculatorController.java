7e4d18da30a25417725e70d119017917a0cf0e23
이지수 <0801ljs@naver.com>
2025-10-19 15:52:24 +0900

    feat: 프로그램 흐름을 제어하는 Controller 구현

---
+++ src/main/java/calculator/controller/CalculatorController.java
@@ -0,0 +1,24 @@
+package calculator.controller;
+
+import calculator.domain.Separator;
+import calculator.domain.StringCalculator;
+import calculator.view.InputView;
+import calculator.view.OutputView;
+
+public class CalculatorController {
+    private final InputView inputView = new InputView();
+    private final OutputView outputView = new OutputView();
+    private final Separator separator = new Separator();
+    private final StringCalculator stringCalculator = new StringCalculator();
+
+    public void run() {
+        try {
+            String expression = inputView.getExpression();
+            String[] numbers = separator.split(expression);
+            int result = stringCalculator.sum(numbers);
+            outputView.printResult(result);
+        } catch (IllegalArgumentException e) {
+            outputView.printError(e.getMessage());
+        }
+    }
+}
